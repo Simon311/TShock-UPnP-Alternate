e83452391579de1cf6b4f4154f06212f31be4bac
Simon <mkontent@gmail.com>
2014-03-07 00:48:19 +0400

    Version 1.0.1 API 1.15

---
+++ UPnP/Main.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using NATUPNPLib;
+
+namespace UPnP
+{
+    [ApiVersion(1, 15)]
+    public class Plugin : TerrariaPlugin
+    {
+        public override Version Version
+        {
+            get { return new Version("1.0.0.0"); }
+        }
+
+        public override string Name
+        {
+            get { return "UPnP-COM"; }
+        }
+
+        public override string Author
+        {
+            get { return "Simon311"; }
+        }
+
+        public override string Description
+        {
+            get { return "Adds UPnP."; }
+        }
+
+        public Plugin(Main game)
+            : base(game)
+        {
+            Order = -1;
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, Start);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, Start);
+                Stop();
+            }
+        }
+
+		#region UPnP
+
+		public static UPnPNAT upnpnat = (UPnPNAT)Activator.CreateInstance(Type.GetTypeFromCLSID(new Guid("AE1E00AA-3FD5-403C-8A27-2BBDC30CD0E1")));
+		public static IStaticPortMappingCollection mappings = upnpnat.StaticPortMappingCollection;
+		public static string portForwardIP;
+		public static int portForwardPort = 7777;
+		public static bool portForwardOpen = false;
+
+		public static bool openPort()
+		{
+			portForwardIP = Netplay.LocalIPAddress();
+			portForwardPort = Netplay.serverPort;
+			if (mappings == null)
+			{
+				Console.WriteLine("(UPnP) Your UPnP discovery is down.");
+				Log.Info("(UPnP) Your UPnP discovery is down.");
+				return false;
+			}
+			try
+			{
+				foreach (IStaticPortMapping staticPortMapping in mappings)
+				{
+					if (staticPortMapping.InternalPort == portForwardPort && staticPortMapping.InternalClient == portForwardIP && staticPortMapping.Protocol == "TCP")
+					{
+						portForwardOpen = true;
+					}
+				}
+				if (!portForwardOpen)
+				{
+					mappings.Add(portForwardPort, "TCP", portForwardPort, portForwardIP, true, "Terraria Server");
+					portForwardOpen = true;
+				}
+				return true;
+			}
+			catch { }
+			return false;
+		}
+
+		public static bool closePort()
+		{
+			try
+			{
+				if (portForwardOpen) mappings.Remove(portForwardPort, "TCP");
+				return true;
+			}
+			catch { }
+			return false;
+		}
+		#endregion UPnP
+
+		private void Start(EventArgs args)
+        {
+			if (openPort())
+			{
+				Console.WriteLine("(UPnP) Port Forward succesful.");
+				Log.Info("(UPnP) Port Forward succesful.");
+			}
+			else
+			{
+				Console.WriteLine("(UPnP) Port Forward failed.");
+				Log.Error("(UPnP) Port Forward failed.");
+			}
+        }
+
+        private void Stop()
+        {
+			if (closePort())
+			{
+				
+				Console.WriteLine("(UPnP) Port Dispose succesful.");
+				Log.Info("(UPnP) Port Dispose succesful.");
+			}
+			else
+			{
+				Console.WriteLine("(UPnP) Port Dispose failed. (WTF?)");
+				Log.Error("(UPnP) Port Dispose failed. (WTF?)");
+			}
+        }
+    }
+}
